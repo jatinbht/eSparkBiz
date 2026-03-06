5bc062a37c6aceb2e130e9a22b2a0efcfd4c6101
Jatin <jatinmihir1915@gmaiil.com>
2026-03-06 19:25:32 +0530

    Project 4: HTML Events

--- html/html-events/index.js
+++ html/html-events/index.js
@@ -1,8 +1,73 @@
-function beforePrint() {
-    console.log("onbeforeprint called");
+function beforePrintHandler() {
+    console.log('onbeforeprint called');
 }
 
-function afterPrint() {
-    console.log("onafterprint called");
+function afterPrintHandler() {
+    console.log('onafterprint called');
 }
+
+const beforeUnloadHandler = () => {
+    event.preventDefault(); //w/o this, not working
+};
+
+const nameInput = document.querySelector('#name');
+
+nameInput.addEventListener('input', (event) => {
+    if (event.target.value !== '') {
+        window.addEventListener('beforeunload', beforeUnloadHandler);
+    } else {
+        window.removeEventListener('beforeunload', beforeUnloadHandler);
+    }
+});
+
+function focusHandler() {
+    console.log('focused');
+}
+
+function blurHandler() {
+    //validate field when user focuses out
+    console.log('blurred');
+}
+
+function focusInHandler() {
+    console.log('focusin')
+}
+function focusOutHandler() {
+    //validate field when user focuses out
+    console.log('focusout')
+}
+
+function inputHandler(){ //generally used w/ sliders
+    console.log('input')
+}
+
+function changeHandler(){
+    console.log('change')
+}
+
+function clickHandler(){
+    console.log('click')
+}
+
+function contextMenuHandler(e){
+    e.preventDefault()
+}
+
+function dlbClickHandler() {
+    console.log('double click')
+}
+
+function copyHandler(event){
+    console.log(event);
+    
+}
+function cutHandler(event){
+    console.log(event);
+    
+}
+function pasteHandler(event){
+    console.log(event);
+    
+}
+
 
