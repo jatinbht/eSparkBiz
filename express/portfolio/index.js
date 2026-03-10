7a370ff3e53000f1fafda7a4428f32086caf245f
Jatin <jatinmihir1915@gmaiil.com>
2026-03-10 15:41:32 +0530

    Integrate EJS in Portfolio

--- express/portfolio/index.js
+++ express/portfolio/index.js
@@ -1,22 +1,39 @@
-import express from "express"
+import express from 'express';
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(import.meta.dirname + '/index.html')
-})
+app.listen(PORT);
+
+app.get('/without-ejs', (req, res) => {
+    res.sendFile(import.meta.dirname + '/index.html');
+});
 
 app.get('/projects/html-events', (req, res) => {
-    res.sendFile(import.meta.dirname + '/public/projects/html-events/index.html')
-})
+    res.sendFile(
+        import.meta.dirname + '/public/projects/html-events/index.html',
+    );
+});
 app.get('/projects/traffic-light', (req, res) => {
-    res.sendFile(import.meta.dirname + '/public/projects/traffic-light/index.html')
-})
+    res.sendFile(
+        import.meta.dirname + '/public/projects/traffic-light/index.html',
+    );
+});
 app.get('/projects/job-applicant-form-v2', (req, res) => {
-    res.sendFile(import.meta.dirname + '/public/projects/job-applicant-form-v2/basic.html')
-})
+    res.sendFile(
+        import.meta.dirname +
+        '/public/projects/job-applicant-form-v2/basic.html',
+    );
+});
 
-app.listen(PORT)
+app.get('/', (req, res) => {
+    const html = res.render('index', { message: 'hello world' });
+});
+
+
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.status(404).render('404');
+    });
