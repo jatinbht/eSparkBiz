ebc05b5511d54edf2f7abaa0c59cfb9259f670b2
jatinbht <jatinmihir1915@gmail.com>
2026-05-27 14:43:13 +0530

    checkpoint: make shadcn work

--- react/job-applicants/client/vite.config.ts
+++ react/job-applicants/client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
+
 
 
 // https://vite.dev/config/
@@ -13,5 +15,10 @@ export default defineConfig({
         // changeOrigin: true
       }
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
