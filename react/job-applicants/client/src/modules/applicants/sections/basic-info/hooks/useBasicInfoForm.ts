59cc88b481770834dfd24d5e9e78c8cbb5641fae
jatinbht <jatinmihir1915@gmail.com>
2026-07-14 18:57:19 +0530

    backup

---
+++ react/job-applicants/client/src/modules/applicants/sections/basic-info/hooks/useBasicInfoForm.ts
@@ -0,0 +1,24 @@
+import { EMPTY_BASIC_INFO } from '@/constants';
+import { CreateBasicInfoSchema } from '@job-applicants/schemas/applicant';
+import { useForm } from '@tanstack/react-form'; // or your specific library
+import { createApplicant } from '../api';
+
+export const useApplicantForm = () => {
+  const form = useForm({
+    defaultValues: EMPTY_BASIC_INFO,
+    validators: {
+      onChange: CreateBasicInfoSchema,
+      onSubmit: CreateBasicInfoSchema,
+    //   onBlur
+    },
+    onSubmit: async ({ value }) => {
+      // console.log('Submitting: ', value);
+      await createApplicant(value);
+    },
+  });
+
+  return form;
+};
+
+// Extract the type from the hook's return type
+export type ApplicantForm = ReturnType<typeof useApplicantForm>;
--- react/job-applicants/todo.md
+++ react/job-applicants/todo.md
@@ -1,2 +1,26 @@
 - [x] add 'shadcn' as table/data-grid: to enable sorting, filtering, searching
 - [ ] Shared API Contracts
+- [ ] learn about microservices architechure:
+
+```md
+server/src/modules/
+
+    applicants/
+        router.ts
+        controller.ts
+        service.ts
+        repository.ts
+
+        basic-info/
+            mapper.ts
+            schema.ts
+
+        education/
+            ...
+
+        experience/
+            ...
+
+        technologies/
+            ...
+```
